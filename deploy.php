3f46bcdad1c4ad0c2768b810a4ac97907532ddef
xenx <atvorozhkov@yandex.ru>
2017-02-24 15:16:03 +0300

    Renew deploy script

--- deploy.php
+++ deploy.php
@@ -1,29 +1,12 @@
 <?php
-    /**
-     * GIT DEPLOYMENT SCRIPT
-     *
-     * Used for automatically deploying websites via GitHub
-     *
-     */
-
-    // array of commands
     $commands = array(
-        'echo $PWD',
-        'whoami',
-        'git pull',
-        'git status',
-        'git submodule sync',
-        'git submodule update',
-        'git submodule status',
+        'git pull'
     );
 
-    // exec commands
+    
     $output = '';
     foreach($commands AS $command){
-        $tmp = shell_exec($command);
-        
-        $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-        $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+        shell_exec($command);
     }
 ?>
 
@@ -31,15 +14,9 @@
 <html lang="en-US">
 <head>
     <meta charset="UTF-8">
-    <title>GIT DEPLOYMENT SCRIPT</title>
+    <title></title>
 </head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<div style="width:700px">
-    <div style="float:left;width:350px;">
-    <p style="color:white;">Git Deployment Script</p>
-	<?php echo $output; ?>
-    </div>
-</div>
+<body>
 </body>
 </html>
 
