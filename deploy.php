c753a7ce4b19295407987a10cef480f7f8417911
xenx <atvorozhkov@yandex.ru>
2017-02-24 15:35:21 +0300

    Deploy now checks secret key

--- deploy.php
+++ deploy.php
@@ -1,22 +1,27 @@
 <?php
-    $commands = array(
-        'git pull'
-    );
+	
+	$hookSecret = getenv('GH_WEBHOOK_SECRET');
 
-    
-    $output = '';
-    foreach($commands AS $command){
-        shell_exec($command);
-    }
+	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		throw new \Exception("HTTP header 'X-Hub-Signature' is missing.");
+	} elseif (!extension_loaded('hash')) {
+		throw new \Exception("Missing 'hash' extension to check the secret code validity.");
+	}
+	list($algo, $hash) = explode('=', $_SERVER['HTTP_X_HUB_SIGNATURE'], 2) + array('', '');
+	if (!in_array($algo, hash_algos(), TRUE)) {
+		throw new \Exception("Hash algorithm '$algo' is not supported.");
+	}
+	$rawPost = file_get_contents('php://input');
+	if ($hash !== hash_hmac($algo, $rawPost, $hookSecret)) {
+		throw new \Exception('Hook secret does not match.');
+	}
+	
+	$commands = array(
+        	'git pull'
+	);
+	
+	foreach($commands AS $command){
+        	shell_exec($command);
+	}
+}
 ?>
-
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-    <meta charset="UTF-8">
-    <title></title>
-</head>
-<body>
-</body>
-</html>
-
