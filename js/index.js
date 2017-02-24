dfb44b04e5b22416c0e2f613d178fdac6f3cfb47
xenx <atvorozhkov@yandex.ru>
2017-02-24 14:16:35 +0300

    Add index page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+/**
+ * Created by xenx on 2/24/17.
+ */
+
+var s = Snap("#loader");
+var bbox = s.getBBox();
+var bigCircle1 = s.circle(100, 100, 10);
+var bigCircle2 = s.circle(150, 100, 10);
+var bigCircle3 = s.circle(200, 100, 10);
