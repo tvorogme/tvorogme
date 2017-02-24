86d05fa38a06ce898a9c0399f2098e00f2bc8d89
xenx <atvorozhkov@yandex.ru>
2017-02-24 16:33:17 +0300

    Add new animation

--- js/index.js
+++ js/index.js
@@ -3,24 +3,93 @@
  */
 
 var s = Snap("#loader");
-var bigCircle1 = s.circle(20, 20, 20);
-var bigCircle2 = s.circle(80, 20, 20);
-var bigCircle3 = s.circle(140, 20, 20);
+var bigCircle1 = s.circle(21, 21, 20);
+var bigCircle2 = s.circle(81, 21, 20);
+var bigCircle3 = s.circle(141, 21, 20);
 
+bigCircle1.attr({
+    fill: "none",
+    stroke: "black"
+});
+
+bigCircle2.attr({
+    fill: "none",
+    stroke: "black"
+});
+
+bigCircle3.attr({
+    fill: "none",
+    stroke: "black"
+});
+
+var state = 0;
 
 function animate_circles() {
-    bigCircle1.animate({r: 5}, 1000);
-    bigCircle2.animate({r: 5}, 2000);
-    bigCircle3.animate({r: 5}, 3000);
+    if (state == 0) {
+        bigCircle1.animate({r: 5}, 500);
+        setTimeout(function () {
+            bigCircle1.animate({r: 20}, 500);
+        }, 500);
 
-    setTimeout(function () {
-        bigCircle1.animate({r: 20}, 1000);
-        bigCircle2.animate({r: 20}, 2000);
-        bigCircle3.animate({r: 20}, 3000);
-    }, 3000);
+
+        bigCircle2.animate({r: 5}, 1000);
+        setTimeout(function () {
+            bigCircle2.animate({r: 20}, 1000);
+        }, 1000);
+
+
+        bigCircle3.animate({r: 5}, 1500);
+        setTimeout(function () {
+            bigCircle3.animate({r: 20}, 1500);
+        }, 1500);
+
+        state = 1;
+    }
+
+    else if (state == 1){
+        bigCircle3.animate({r: 5}, 500);
+        setTimeout(function () {
+            bigCircle3.animate({r: 20}, 500);
+        }, 500);
+
+
+        bigCircle2.animate({r: 5}, 1000);
+        setTimeout(function () {
+            bigCircle2.animate({r: 20}, 1000);
+        }, 1000);
+
+
+        bigCircle1.animate({r: 5}, 1500);
+        setTimeout(function () {
+            bigCircle1.animate({r: 20}, 1500);
+        }, 1500);
+
+        state = 2;
+    }
+
+
+    else if (state == 2){
+        bigCircle2.animate({r: 5}, 500);
+        setTimeout(function () {
+            bigCircle2.animate({r: 20}, 500);
+        }, 500);
+
+
+        bigCircle3.animate({r: 5}, 1000);
+        setTimeout(function () {
+            bigCircle3.animate({r: 20}, 1000);
+        }, 1000);
+
+
+        bigCircle1.animate({r: 5}, 1500);
+        setTimeout(function () {
+            bigCircle1.animate({r: 20}, 1500);
+        }, 1500);
+
+        state = 0;
+    }
 }
 
 animate_circles();
 
-setInterval(animate_circles, 6000);
-
+setInterval(animate_circles, 3000);
