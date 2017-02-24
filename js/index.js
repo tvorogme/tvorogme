773ac70bb03b2e1a82eec29d0a8bb8468457b920
xenx <atvorozhkov@yandex.ru>
2017-02-24 16:14:07 +0300

    Add circles animation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,24 @@
  */
 
 var s = Snap("#loader");
-var bbox = s.getBBox();
 var bigCircle1 = s.circle(20, 20, 20);
 var bigCircle2 = s.circle(80, 20, 20);
 var bigCircle3 = s.circle(140, 20, 20);
+
+
+function animate_circles() {
+    bigCircle1.animate({r: 5}, 1000);
+    bigCircle2.animate({r: 5}, 2000);
+    bigCircle3.animate({r: 5}, 3000);
+
+    setTimeout(function () {
+        bigCircle1.animate({r: 20}, 1000);
+        bigCircle2.animate({r: 20}, 2000);
+        bigCircle3.animate({r: 20}, 3000);
+    }, 3000);
+}
+
+animate_circles();
+
+setInterval(animate_circles, 6000);
+
